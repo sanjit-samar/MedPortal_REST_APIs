03151c77ab7b5697afd687b05dfc45bbd48d628b
sanjit samar <sanjitsamar07@gmail.com>
2025-11-01 00:23:25 +0530

    added login route

--- src/Controllers/authController.js
+++ src/Controllers/authController.js
@@ -1,5 +1,7 @@
 const User = require("../Model/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const user = require("../Model/user");
 
 const register = async (req, res) => {
   const { username, email, password, role } = req.body;
@@ -53,31 +55,73 @@ const register = async (req, res) => {
   }
 };
 
-// const login = async (req,res)=>{
-//   const {email, username, password} = req.body;
+const login = async (req, res) => {
+  const { email, username, password } = req.body;
 
-//     // Validate the user fields
-//   if (!username || !email || !password) {
-//     res.status(400).json({
-//       status: 400,
-//       success: false,
-//       message: "Please fill username/email with password",
-//     });
-//   }
+  // Validate the user fields
+  if (!username || !email || !password) {
+    res.status(400).json({
+      status: 400,
+      success: false,
+      message: "Please fill username/email with password",
+    });
+  }
 
-// try{
-//     //check user is registered or not
-//   const userExists = User.findById({
-//     _id :
-//   })
+  try {
+    //check user is registered or not
+    const userExists = User.findOne({
+      $or: [{ username, email }],
+    });
+    if (!userExists) {
+      res.status(409).json({
+        success: false,
+        status: 409,
+        message: "User Not Exists. Register First",
+      });
+    }
 
-// }catch(error){
-//  res.status(500).json({
-//       status: 500,
-//       success: false,
-//       message: "Internal Server error",
-//     });
-// }
+    const isPasswordMatch = await bcrypt.compare(password, userExists.password);
+    if (!isPasswordMatch) {
+      res.status(404).json({
+        success: false,
+        status: 404,
+        message: "Incorrect Password",
+      });
+    }
 
-// }
-module.exports = { register };
+    //create jwt token to store locally or in cookies
+    const token = jwt.sign(
+      {
+        userid: userExists._id,
+        username: userExists.username,
+        role: userExists.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "1d" }
+    );
+
+    //set token in HTTP-only cookies or send it to res so in local/session storage
+    //but here storing in cookies
+
+    res.cookies("token", token, {
+      httpOnly: true,
+      //secure: process.env.NODE_ENV === "production", // set true in production
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
+
+    res.status(200).json({
+       status: 200,
+      success: true,
+      message: "Logged In successfully",
+      token
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 500,
+      success: false,
+      message: "Internal Server error",
+    });
+  }
+};
+module.exports = { register,login };
--- src/Routes/authroutes.js
+++ src/Routes/authroutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const Router = express.Router();
-const {register} = require("../Controllers/authController");
+const {register, login} = require("../controllers/authController");
 
 
 Router.post("/register", register);
-//Router.post("/login",);
+Router.post("/login", login);
 
 module.exports = Router;
